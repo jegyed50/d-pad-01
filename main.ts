16c97109ab5396dc06de381de22aa428b279c97a
Egyed József <jegyed50@gmail.com>
2023-05-28 15:57:38 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_4_DOWN, function () {
+    basic.showString("A")
+    basic.pause(500)
+    basic.clearScreen()
+})
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
 basic.forever(function () {
 	
 })
